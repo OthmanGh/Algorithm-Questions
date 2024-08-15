e3954f78172f992aec2d559a1269e368dbf7b891
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-15 14:16:32 +0300

    Implement LinkedList data structure

---
+++ linkedList.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  removeNodes(num) {}
+}
