344b73137731deada0537dca8b5a6a949c09d216
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-15 16:15:39 +0300

    Implement print all method

--- linkedList.js
+++ linkedList.js
@@ -23,4 +23,29 @@ class LinkedList {
   }
 
   removeNodes(num) {}
+
+  printAll() {
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+
+    console.log(null);
+  }
 }
+
+const ll = new LinkedList();
+
+ll.add(10);
+ll.add(5);
+ll.add(12);
+ll.add(7);
+ll.add(3);
+ll.add(9);
+ll.add(10);
+
+ll.removeNodes(7);
+
+ll.printAll();
